1fb7e4946db4442f15c54fc48e7fb3c8b458d51a
JaumeGM97 <jaumegual@paucasesnovescifp.cat>
2026-02-13 17:35:02 +0100

    module done

---
+++ models/shelter.js
@@ -0,0 +1,11 @@
+class Pet {
+    constructor(id, nombre, especie, edad, estaVacunado) {
+        this.id;
+        this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
+        this.estaVacunado = estaVacunado;
+    }
+}
+
+module.exports = Pet
